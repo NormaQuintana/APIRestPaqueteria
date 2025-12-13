f073183527e348f22ece58b174809ac5c9f68be3
NormaQuintana <NormaQuintana@gmail.com>
2025-12-13 15:29:01 -0600

    feat: Implementación completa de la entidad Unidad (POJO, Mapper, Dominio y WS)

---
+++ src/java/dominio/UnidadImp.java
@@ -0,0 +1,147 @@
+package dominio;
+
+import dto.Respuesta;
+import java.util.List;
+import modelo.mybatis.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import pojo.EntidadesPrincipales.Unidad;
+import utilidades.Constantes;
+
+public class UnidadImp {
+
+    public static List<Unidad> obtenerUnidades(){
+        List<Unidad> unidades = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                unidades = conexionBD.selectList("unidad.obtener-todas");
+                conexionBD.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return unidades;
+    }
+    
+    public static Respuesta registrar(Unidad unidad){
+        Respuesta respuesta = new Respuesta();
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                Integer existeVin = conexionBD.selectOne("unidad.verificar-vin", unidad);
+                if (existeVin != null && existeVin > 0) {
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Ya existe una unidad registrada con el mismo VIN.");
+                    return respuesta;
+                }
+
+                Integer existeNii = conexionBD.selectOne("unidad.verificar-nii", unidad);
+                if (existeNii != null && existeNii > 0) {
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Ya existe una unidad registrada con el mismo Número de Identificación Interno.");
+                    return respuesta;
+                }
+
+                int filasAfectadas = conexionBD.insert("unidad.registrar", unidad);
+
+                if(filasAfectadas > 0){
+                    conexionBD.commit();
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Unidad: " + unidad.getNoIdentificacion() + " registrada correctamente");
+                }else{
+                    conexionBD.rollback();
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Lo sentimos, la unidad no fue agregada.");
+                }
+            }catch(Exception e){
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
+            } finally {
+                 conexionBD.close();
+            }
+        }else{
+            respuesta.setError(true);
+            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
+        }
+        return respuesta;
+    }
+    
+    public static Respuesta editarUnidad(Unidad unidad){
+        Respuesta respuesta = new Respuesta();
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                Integer existeNii = conexionBD.selectOne("unidad.verificar-nii-edicion", unidad);
+                if (existeNii != null && existeNii > 0) {
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Ya existe otra unidad registrada con el mismo Número de Identificación Interno.");
+                    return respuesta;
+                }
+
+                int filasAfectadas = conexionBD.update("unidad.editar", unidad);
+
+                if(filasAfectadas > 0){
+                    conexionBD.commit();
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Unidad con NII: " + unidad.getNoIdentificacion() + " actualizada correctamente.");
+                }else{
+                    conexionBD.rollback();
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Lo sentimos, la unidad no fue editada. Verifique la información.");
+                }
+            }catch(Exception e){
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
+            } finally {
+                 conexionBD.close();
+            }
+        }else{
+            respuesta.setError(true);
+            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
+        }
+        return respuesta;
+    }
+ 
+    public static Respuesta darBajaUnidad(Unidad unidad){
+        Respuesta respuesta = new Respuesta();
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                int filasAfectadas = conexionBD.update("unidad.dar-baja", unidad);
+
+                if(filasAfectadas > 0){
+                    conexionBD.commit();
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Unidad dada de baja exitosamente.");
+                }else{
+                    conexionBD.rollback();
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Lo sentimos, no se encontró la unidad con ese ID.");
+                }
+            }catch(Exception e){
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
+            } finally {
+                 conexionBD.close();
+            }
+        }else{
+            respuesta.setError(true);
+            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
+        }
+        return respuesta;
+    }
+    
+    public static List<Unidad> buscarUnidades(Unidad unidad){
+    List<Unidad> unidades = null;
+    SqlSession conexionBD = MyBatisUtil.getSession();
+    if(conexionBD != null){
+        try{
+            unidades = conexionBD.selectList("unidad.buscar-unidades", unidad);
+            conexionBD.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+        return unidades;
+    }
+}
---
+++ src/java/pojo/EntidadesPrincipales/Unidad.java
@@ -0,0 +1,126 @@
+package pojo.EntidadesPrincipales;
+
+import java.util.Date; 
+import pojo.Catalogo.TipoUnidad;
+
+public class Unidad {
+
+    private Integer idUnidad;
+    private String marca;
+    private String modelo;
+    private Integer anio;
+    private String vin;
+    private String noIdentificacion; 
+    private Boolean status;
+    private String motivoBaja;
+    private Date fechaBaja;
+    private TipoUnidad tipoUnidad; 
+    private Colaborador conductor; 
+
+    public Unidad() {
+    
+    }
+
+    public Unidad(Integer idUnidad, String marca, String modelo, Integer anio, String vin, String noIdentificacion, Boolean status, String motivoBaja, Date fechaBaja, TipoUnidad tipoUnidad, Colaborador conductor) {
+        this.idUnidad = idUnidad;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.vin = vin;
+        this.noIdentificacion = noIdentificacion;
+        this.status = status;
+        this.motivoBaja = motivoBaja;
+        this.fechaBaja = fechaBaja;
+        this.tipoUnidad = tipoUnidad;
+        this.conductor = conductor;
+    }
+
+    public Integer getIdUnidad() {
+        return idUnidad;
+    }
+
+    public void setIdUnidad(Integer idUnidad) {
+        this.idUnidad = idUnidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getNoIdentificacion() {
+        return noIdentificacion;
+    }
+
+    public void setNoIdentificacion(String noIdentificacion) {
+        this.noIdentificacion = noIdentificacion;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getMotivoBaja() {
+        return motivoBaja;
+    }
+
+    public void setMotivoBaja(String motivoBaja) {
+        this.motivoBaja = motivoBaja;
+    }
+
+    public Date getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(Date fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+
+    public TipoUnidad getTipoUnidad() {
+        return tipoUnidad;
+    }
+    
+    public void setTipoUnidad(TipoUnidad tipoUnidad) {
+        this.tipoUnidad = tipoUnidad;
+    }
+
+    public Colaborador getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Colaborador conductor) {
+        this.conductor = conductor;
+    }
+
+}
