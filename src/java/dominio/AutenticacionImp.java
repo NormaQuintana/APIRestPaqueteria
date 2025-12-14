a0b09176fa2d5ce27de8c0892d3825b485e75453
NormaQuintana <NormaQuintana@gmail.com>
2025-12-13 23:55:44 -0600

    Autenticacion Conductor

--- src/java/dominio/AutenticacionImp.java
+++ src/java/dominio/AutenticacionImp.java
@@ -34,4 +34,31 @@ public class AutenticacionImp {
         }
         return autenticacion;
     }
+    
+    public static RSAutenticacion autenticarConductor(String noPersonal, String password){
+        RSAutenticacion autenticacion = new RSAutenticacion();
+        autenticacion.setError(true);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                HashMap<String, String> parametros = new LinkedHashMap<>();
+                parametros.put("noPersonal", noPersonal);
+                parametros.put("password", password);
+                Colaborador colaborador = conexionBD.selectOne("autenticacion.conductor", parametros);
+                if(colaborador != null){
+                    autenticacion.setError(false);
+                    autenticacion.setMensaje("Credenciales correctas del usuario: "+colaborador.getNombre());
+                    autenticacion.setColaborador(colaborador);
+                }else{
+                    autenticacion.setMensaje("Credenciales incorrectas, por favor verifique la informacion");
+                }
+            }catch(Exception e){
+                autenticacion.setMensaje(e.getMessage());
+            }
+            conexionBD.close();
+        }else{
+            autenticacion.setMensaje("Lo sentimos por el momento no hay conexion a los datos");
+        }
+        return autenticacion;
+    }
 }
