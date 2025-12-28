654f46d68f55ec9901ae2ac81808eb548c947358
karen <karenyoval26@gmail.com>
2025-12-28 14:13:51 -0600

    Cambios en mappers, constantes y servicios WS

--- src/java/ws/ApplicationConfig.java
+++ src/java/ws/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
         resources.add(ws.ClienteWS.class);
         resources.add(ws.ColaboradorWS.class);
         resources.add(ws.EnvioWS.class);
+        resources.add(ws.GeografiaWS.class);
         resources.add(ws.PaqueteWS.class);
         resources.add(ws.SucursalWS.class);
         resources.add(ws.UnidadWS.class);
--- src/java/ws/ColaboradorWS.java
+++ src/java/ws/ColaboradorWS.java
@@ -17,92 +17,92 @@ import pojo.EntidadesPrincipales.Colaborador;
 
 @Path("colaborador")
 public class ColaboradorWS {
-    
+
     @Path("obtener-todos")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerColaboradores(){
+    public List<Colaborador> obtenerColaboradores() {
         return ColaboradorImp.obtenerColaboradores();
     }
-    
+
     @Path("obtener-por-rol/{idRol}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerColaboradoresPorRol(@PathParam("idRol") Integer idRol){
+    public List<Colaborador> obtenerColaboradoresPorRol(@PathParam("idRol") Integer idRol) {
         return ColaboradorImp.obtenerColaboradoresPorRol(idRol);
     }
-    
-    @Path("obtener-por-noPerosnal/{noPersonal}")
+
+    @Path("obtener-por-noPersonal/{noPersonal}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Colaborador obtenerColaboradoresPorNoPersonal(@PathParam("noPersonal") String noPersonal){
+    public Colaborador obtenerColaboradoresPorNoPersonal(@PathParam("noPersonal") String noPersonal) {
         return ColaboradorImp.obtenerColaboradoresPorNoPersonal(noPersonal);
     }
-    
+
     @Path("obtener-por-nombre/{nombre}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerColaboradoresPorNombre(@PathParam("nombre") String nombre){
+    public List<Colaborador> obtenerColaboradoresPorNombre(@PathParam("nombre") String nombre) {
         return ColaboradorImp.obtenerColaboradoresPorNombre(nombre);
     }
-    
+
     @Path("registrar")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Respuesta registrarColaborador(String json){
+    public Respuesta registrarColaborador(String json) {
         Gson gson = new Gson();
-        try{
+        try {
             Colaborador colaborador = gson.fromJson(json, Colaborador.class);
             return ColaboradorImp.registrarColaborador(colaborador);
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new BadRequestException(e.getMessage());
         }
     }
-    
+
     @Path("editar")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Respuesta editarColaborador(String json){
+    public Respuesta editarColaborador(String json) {
         Gson gson = new Gson();
-        try{
+        try {
             Colaborador colaborador = gson.fromJson(json, Colaborador.class);
             return ColaboradorImp.editar(colaborador);
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new BadRequestException(e.getMessage());
         }
     }
-    
+
     @Path("eliminar/{idColaborador}")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
-    public Respuesta eliminarColaborador(@PathParam("idColaborador") Integer idColaborador){
-        try{
+    public Respuesta eliminarColaborador(@PathParam("idColaborador") Integer idColaborador) {
+        try {
             return ColaboradorImp.eliminarColaborador(idColaborador);
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new BadRequestException(e.getMessage());
         }
     }
-    
+
     @Path("subir-foto/{idColaborador}")
     @PUT
-    @Produces (MediaType.APPLICATION_JSON)
-     public Respuesta subirFoto(@PathParam ("idColaborador") Integer idColaborador, byte[] foto){
-        if(idColaborador != null && idColaborador > 0 && foto.length > 0){
+    @Produces(MediaType.APPLICATION_JSON)
+    public Respuesta subirFoto(@PathParam("idColaborador") Integer idColaborador, byte[] foto) {
+        if (idColaborador != null && idColaborador > 0 && foto.length > 0) {
             return ColaboradorImp.registrarFoto(idColaborador, foto);
         }
         throw new BadRequestException();
     }
-     
+
     @Path("obtener-foto/{idColaborador}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Colaborador obtenerFoto(@PathParam("idColaborador") Integer idColaborador){
-        if(idColaborador != null && idColaborador > 0){
+    public Colaborador obtenerFoto(@PathParam("idColaborador") Integer idColaborador) {
+        if (idColaborador != null && idColaborador > 0) {
             return ColaboradorImp.obtenerFoto(idColaborador);
         }
-        throw new BadRequestException(); 
-         
+        throw new BadRequestException();
+
     }
 }
